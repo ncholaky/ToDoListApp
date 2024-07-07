ae6bb5900ded6eb0bff89c11f4655803b5a3b8e1
Nicoletta <nicoletta.cholaky@gmail.com>
2024-07-07 14:48:36 +0200

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Nicoletta Cholaky on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
