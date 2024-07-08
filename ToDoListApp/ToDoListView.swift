4d3b58429e1151a4cf385635f0151ae639495f71
Nicoletta <nicoletta.cholaky@gmail.com>
2024-07-08 11:11:22 +0200

    Deleted the previous ForEach and inserted one that loops through toDos, an array of strings.  Each one leads to DetailView() with its string value

--- ToDoListApp/ToDoListView.swift
+++ ToDoListApp/ToDoListView.swift
@@ -9,28 +9,29 @@ import SwiftUI
 
 struct ToDoListView: View {
     var body: some View {
+        var toDos = ["Learn Swift", "Build Apps", "Save the World", "Bring the Awesome", "Take a Vacation"]
         
         NavigationStack{
             List {
-                ForEach(0..<100, id:\.self) {
-                    number in
-                    NavigationLink {
-                        DetailView(passedValue: "Item \(number)")
+                ForEach(toDos, id:\.self){
+                    toDo in
+                    NavigationLink{
+                        DetailView(passedValue: toDo)
                     } label: {
-                        Text("Item \(number)")
+                        Text(toDo)
                     }
-                    
+                   
                 }
             }
             //navigationTitle must be added inside a NavigationStack, NOT BELOW IT
-            .navigationTitle("School Year")
+            .navigationTitle("To Do List")
             //This is the default, just to know it can be changed
             .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.automatic/*@END_MENU_TOKEN@*/)
             .listStyle(.plain
             )
-            }
         }
     }
+}
 
 
 #Preview {
